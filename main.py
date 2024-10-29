00f38b9aaf53927e822821431e581fae3c721bfb
Frank Fajardo <105552462+frankfajardo1978@users.noreply.github.com>
2024-10-29 05:21:27 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+import pandas as pd
+from datetime import datetime
+
+app = FastAPI()
+
+# Cargar el archivo CSV al iniciar la aplicación
+data = pd.read_csv("archivotransformado.csv")
+
+@app.get("/")
+def cantidad_filmaciones_mes(mes: str):
+    # Convertir el mes a minúsculas para evitar problemas de mayúsculas
+    mes = mes.lower()
+    
+    # Diccionario para convertir el nombre del mes en español al número correspondiente
+    meses = {
+        "enero": 1, "febrero": 2, "marzo": 3, "abril": 4,
+        "mayo": 5, "junio": 6, "julio": 7, "agosto": 8,
+        "septiembre": 9, "octubre": 10, "noviembre": 11, "diciembre": 12
+    }
+    
+    # Validar si el mes ingresado es válido
+    if mes not in meses:
+        return {"error": "Mes ingresado no válido. Por favor, ingrese un mes en español."}
+    
+    # Obtener el número del mes correspondiente
+    numero_mes = meses[mes]
+    
+    # Convertir la columna de fechas al tipo datetime si es necesario
+    data['release_date'] = pd.to_datetime(data['release_date'], errors='coerce')
+    
+    # Filtrar las películas estrenadas en el mes especificado
+    peliculas_mes = data[data['release_date'].dt.month == numero_mes]
+    cantidad = len(peliculas_mes)
+    
+    return {"mensaje": f"{cantidad} cantidad de películas fueron estrenadas en el mes de {mes.capitalize()}"}
